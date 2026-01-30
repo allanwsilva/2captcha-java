55c275e6c710480020c87c6ac53d433a4ab03758
victorkowalski <vkowalski@yandex.ru>
2026-01-30 15:30:44 +0300

    added params

--- src/main/java/com/twocaptcha/captcha/AmazonWaf.java
+++ src/main/java/com/twocaptcha/captcha/AmazonWaf.java
@@ -18,8 +18,20 @@ public class AmazonWaf extends Captcha {
     public void setIV(String iv) {
         params.put("iv", iv);
     }
-    
+
     public void setContext(String context) {
         params.put("context", context);
+    }
+
+    public void setChallengeScript(String challengeScript) {
+        params.put("challenge_script", challengeScript);
+    }
+
+    public void setCaptchaScript(String captchaScript) {
+        params.put("captcha_script", captchaScript);
+    }
+
+    public void setHeaderAcao(Integer headerAcao) {
+        params.put("header_acao", java.lang.String.valueOf(headerAcao));
     }
 }
